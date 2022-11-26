577ae65c2588fccb6702d68ac1961036f1e68e65
Oleg Stepanenko <o.stepanenko@inostudio.com>
2022-11-27 00:47:17 +0300

    create onboarding page

--- app-case/src/routes/routes.js
+++ app-case/src/routes/routes.js
@@ -1,18 +1,23 @@
 import MainPage from "../pages/MainPage";
 import AuthPage from "../pages/AuthPage";
 import Donut from "../pages/Donut";
+import Onboarding from "../pages/Onboarding";
 
 export const publicRoutes = [
-	{
-		path: "/",
-		component: <MainPage />,
-	},
-	{
-		path: "/login",
-		component: <AuthPage />,
-	},
-	{
-		path: "/donut",
-		component: <Donut />,
-	},
+  {
+    path: "/",
+    component: <MainPage />,
+  },
+  {
+    path: "/login",
+    component: <AuthPage />,
+  },
+  {
+    path: "/donut",
+    component: <Donut />,
+  },
+  {
+    path: "/onboarding",
+    component: <Onboarding />,
+  },
 ];
