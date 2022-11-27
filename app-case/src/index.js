ccc428de9f2de5d3d149e9332b23b2d89307b400
borodos <stepllerrus78@gmail.com>
2022-11-27 09:33:50 +0300

    Onbording done(50%)

--- app-case/src/index.js
+++ app-case/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 import Card from "./component/Card";
--- app-case/src/pages/Onboarding.js
+++ app-case/src/pages/Onboarding.js
@@ -1,83 +1,191 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "../css/onboarding.css";
 import "./cardStyle.css";
 import donut from "../assets/donut.png";
 import pic1 from "../assets/pic1_1.png";
+import pic2 from "../assets/Pic1_2.png";
+import pic3 from "../assets/Pic1_3.png";
+import pic4 from "../assets/Pic1_4.png";
+import DonutYES from "../assets/DonutYES.png";
 import man from "../assets/man.png";
 import manGray from "../assets/man-gray.png";
 import exit from "../assets/exit.png";
 import check from "../assets/check.png";
 
 function Onboarding() {
-  return (
-    <div className="onboarding-page onboarding">
-      <header className="donut-page__header">
-        <div className="donut-page__header header-donut">
-          <div className="header-donut__profile profile">
-            <div className="profile__img"></div>
-            <div className="profile__name">Иван Иванов</div>
-          </div>
-          <div className="header-donut__buttons ">
-            <button
-              disabled
-              className="header-donut__button header-donut__button-active"
-            >
-              <div className="header-donut__text">Раздел 1 - завершен</div>
-              <div className="header-donut__btn-img"></div>
-            </button>
-            <button disabled className="header-donut__button">
-              <div className="header-donut__text">Раздел 2 - Донат встречи</div>
-            </button>
-          </div>
-          <button className="exit-btn">
-            <img src={exit} alt="" />
-          </button>
-        </div>
-      </header>
-      <main className="onboarding__content">
-        <div className="onboarding__donut">
-          <div className="onboarding__donut-img">
-            <img src={donut} alt=" пончик" />
-          </div>
-          <div className="onboarding__donut__text">X</div>
-          <div className="onboarding__donut__count">1</div>
-        </div>
-        <div className="onboarding__container">
-          <div className="onboarding__person persone">
-            <div className="persone__img">
-              <img src={man} alt="" />
-            </div>
-            <div className="persone__name">Балабуйко Екатерина</div>
-            <div className="persone__description">
-              Знакомься! Это наш главный ментор. Она будет помогать тебе на
-              ранних этапах работы в проекте, направлять на верные пути решения
-              задач и учить тебя писать только хороший код. В свободное время
-              она обычно играет на рояле, а ещё она любит сухое вино.
-            </div>
-            <div className="onboarding__decor">
-              <img src={pic1} alt="" />
-            </div>
-          </div>
-          <div className="onboarding__progress">
-            <ul className="step">
-              <li className="step__item">
-                <div className="step__item-active">
-                  <img src={manGray} alt="" />
-                </div>
-              </li>
-              <li className="step__item">
-                <img src={check} alt="" />
-              </li>
-              <li className="step__item"></li>
-              <li className="step__item">
-                <img src={check} alt="" />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+	const [donuts, setDonuts] = useState(0);
+
+	const [show, setShow] = useState(true);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	return (
+		<div className="onboarding-page onboarding">
+			<header className="donut-page__header">
+				<div className="donut-page__header header-donut">
+					<div className="header-donut__profile profile">
+						<div className="profile__img"></div>
+						<div className="profile__name">Иван Иванов</div>
+					</div>
+					<div className="header-donut__buttons ">
+						<button
+							disabled
+							className="header-donut__button header-donut__button-active"
+						>
+							<div className="header-donut__text">Раздел 1 - завершен</div>
+							<div className="header-donut__btn-img"></div>
+						</button>
+						<button disabled className="header-donut__button">
+							<div className="header-donut__text">Раздел 2 - Донат встречи</div>
+						</button>
+					</div>
+					<button className="exit-btn">
+						<img src={exit} alt="" />
+					</button>
+				</div>
+			</header>
+			<main className="onboarding__content">
+				<div className="onboarding__donut">
+					<div className="onboarding__donut-img">
+						<img src={donut} alt=" пончик" />
+					</div>
+					<div className="onboarding__donut__text">X</div>
+					<div className="onboarding__donut__count">{donuts + 1}</div>
+				</div>
+				<div
+					className="onboarding__container"
+					onClick={() => setDonuts(donuts + 1)}
+				>
+					{donuts === 0 ? (
+						<div className="onboarding__person persone">
+							<div className="persone__img">
+								<img src={man} alt="" />
+							</div>
+							<div className="persone__name">Балабуйко Екатерина</div>
+							<div className="persone__description">
+								Знакомься! Это наш главный ментор. Она будет помогать тебе на
+								ранних этапах работы в проекте, направлять на верные пути
+								решения задач и учить тебя писать только хороший код. В
+								свободное время она обычно играет на рояле, а ещё она любит
+								сухое вино.
+							</div>
+							<div className="onboarding__decor">
+								<img src={pic1} alt="" />
+							</div>
+						</div>
+					) : donuts === 1 ? (
+						<div className="onboarding__person persone">
+							<div className="persone__img">
+								<img src={man} alt="" />
+							</div>
+							<div className="persone__name">Калиткина Анна</div>
+							<div className="persone__description">
+								А это Team Lead – невероятно умный и общительный человек. Она
+								вместе с менеджерами проектов взаимодействует с заказчиками, а
+								также занимается проектированием и созданием архитектуры
+								проектов. Надеемся, однажды ты будешь таким же опытным и
+								самостоятельным
+							</div>
+							<div className="onboarding__decor">
+								<img src={pic2} alt="" />
+							</div>
+						</div>
+					) : donuts === 2 ? (
+						<div className="onboarding__person persone">
+							<div className="persone__img">
+								<img src={man} alt="" />
+							</div>
+							<div className="persone__name">Дмитриев Владимир</div>
+							<div className="persone__description">
+								Молодой парень, а уже главные менеджер проектов. Он является
+								связующим звеном между заказчиками, аналитиками, разработчиками,
+								тестировщиками и остальными людьми, ведущими работу над проектом
+							</div>
+							<div className="onboarding__decor">
+								<img src={pic3} alt="" />
+							</div>
+						</div>
+					) : donuts === 3 ? (
+						<div className="onboarding__person persone">
+							<div className="persone__img">
+								<img src={man} alt="" />
+							</div>
+							<div className="persone__name">Сергеева Ирина</div>
+							<div className="persone__description">
+								Она занимается как подбором новых сотрудников, так и
+								взаимодействием с уже работающими. А ещё она помогает в
+								организации мероприятий (корпоративов, кино-вечеров и многих
+								других).
+							</div>
+							<div className="onboarding__decor">
+								<img src={pic4} alt="" />
+							</div>
+						</div>
+					) : (
+						<>
+							<div className="onboarding__person persone">
+								<div className="persone__img">
+									<img src={man} alt="" />
+								</div>
+								<div className="persone__name">Сергеева Ирина</div>
+								<div className="persone__description">
+									Она занимается как подбором новых сотрудников, так и
+									взаимодействием с уже работающими. А ещё она помогает в
+									организации мероприятий (корпоративов, кино-вечеров и многих
+									других).
+								</div>
+								<div className="onboarding__decor">
+									<img src={pic4} alt="" />
+								</div>
+							</div>
+							<Modal
+								style={{
+									width: "360px",
+									marginLeft: "620px",
+									marginTop: "100px",
+									borderRadius: "36px",
+								}}
+								backdrop="static"
+								show={show}
+								onHide={handleClose}
+							>
+								<div className="modal__container">
+									<h1>Поздравляем!</h1>
+									<div className="modal__img">
+										<img src={DonutYES} alt="Donut" />
+									</div>
+									<div className="modal__text">
+										Ты собрал все части пончика, отправляйся на следующий этап,
+										для того чтобы слопать его с коллегами.
+									</div>
+									<button className="modal__button">Погнали!</button>
+								</div>
+							</Modal>
+						</>
+					)}
+
+					<div className="onboarding__progress">
+						<ul className="step">
+							<li className="step__item">
+								<div className="step__item-active">
+									<img src={manGray} alt="" />
+								</div>
+							</li>
+							<li className="step__item">
+								<img src={check} alt="" />
+							</li>
+							<li className="step__item"></li>
+							<img className="last__img" src={check} alt="" />
+
+							<div className="last__step__item"></div>
+						</ul>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default Onboarding;
